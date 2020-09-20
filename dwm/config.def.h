1d923bc3873482b9c120c5d8067022c0de9c9507
Hamish <hamishdehmer@gmail.com>
2020-09-20 11:06:07 -0700

    Changed DWM browser and bar keybinds.

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -129,7 +129,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
         { MODKEY,                       XK_n,      spawn,          {.v = fbrowser } },
         { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = slock } },
-	{ MODKEY|ShiftMask,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
